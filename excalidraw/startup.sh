e62d8dd8f63b34b27e43105a17264317650aea99
Janne Enberg <janne.enberg@lietu.net>
2024-02-22 19:08:48 +0200

    Slightly more verbose script output

--- excalidraw/startup.sh
+++ excalidraw/startup.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+echo "Building application..."
 yarn build:app:docker
+
+echo "Publishing build results..."
 rm -rf /var/www/html/*
 cp -a build/* /var/www/html
 
-sed -i '/\[::\]:80/d' /etc/nginx/sites-enabled/default
-
 echo "Starting Nginx..."
+sed -i '/\[::\]:80/d' /etc/nginx/sites-enabled/default
 nginx -g "daemon off;"
